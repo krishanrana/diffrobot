2081dbd78bab96b15aa85be8cf1c5521decc38e0
krishanrana <krishanrana100@gmail.com>
2024-02-27 23:17:03 +1000

    working state

--- diffrobot/diffusion_policy/inference_real_robot_state.py
+++ diffrobot/diffusion_policy/inference_real_robot_state.py
@@ -32,76 +32,17 @@ from reactivex import operators as ops
 from multiprocessing.managers import SharedMemoryManager
 from diffrobot.calibration.aruco_detector import ArucoDetector, aruco
 from diffrobot.realsense.single_realsense import SingleRealsense
+from diffrobot.diffusion_policy.diffusion_policy import DiffusionPolicy
 
 
+# empty cache
+torch.cuda.empty_cache()
 
-
-class FixedCropTransform:
-    def __init__(self, top, left, height, width):
-        self.top = top
-        self.left = left
-        self.height = height
-        self.width = width
-
-    def __call__(self, img):
-        return crop(img, self.top, self.left, self.height, self.width)
-
-
-
-# agent_pos is 2 dimensional
-lowdim_obs_dim = 2
-# observation feature has 514 dims in total per step
-obs_dim = 6
-action_dim = 2
-
-# parameters
-pred_horizon = 16
-obs_horizon = 2
-action_horizon = 10
-
-# num_diffusion_iters = 100
-num_diffusion_iters = 100
-noise_scheduler = DDPMScheduler(
-    num_train_timesteps=num_diffusion_iters,
-    # the choise of beta schedule has big impact on performance
-    # we found squared cosine works the best
-    beta_schedule='squaredcos_cap_v2',
-    # clip output to [-1,1] to improve stability
-    clip_sample=True,
-    # our network predicts noise (instead of denoised action)
-    prediction_type='epsilon')
-
-device = torch.device('cuda')
-
-
-noise_pred_net = ConditionalUnet1D(
-    input_dim=action_dim,
-    global_cond_dim=6
-)
-
-ckpt_path = "saved_weights/nets_reacher_state_best.ckpt"
-state_dict = torch.load(ckpt_path, map_location='cuda')
-
-
-_ = noise_pred_net.to(device)
-
-ema_nets = noise_pred_net
-ema_nets.load_state_dict(state_dict)
-print('Pretrained weights loaded.')
-
-#read stats pkl
-stats_path = "saved_weights/stats.pkl"
-with open(stats_path, 'rb') as f:
-    stats = pkl.load(f)
-
-
-#@markdown ### **Inference**
-
-# limit enviornment interaction to 200 steps before termination
-max_steps = 2000
-
-
-################################################################################
+policy = DiffusionPolicy(mode='infer', 
+                        policy_type='state', 
+                        config_file='config_state_pretrain', 
+                        finetune=False, 
+                        saved_run_name='distinctive-star-4_state')
 
 
 # Setup cameras
@@ -113,20 +54,13 @@ cam.start()
 marker_detector = ArucoDetector(cam, 0.05, aruco.DICT_4X4_50, 8, visualize=False)
 
 cam.set_exposure(exposure=100, gain=60)
-
-
 time.sleep(1.0)
-
-# vis = MultiCameraVisualizer(cams, row=2, col=1)
-# vis.start()
-# time.sleep(1.0)
 
 
 print("Camera setup complete")
 # Setup robot
 panda = Robot("172.16.0.2")
 panda.set_dynamic_rel(0.4, accel_rel=0.005, jerk_rel=0.005)
-
 panda.move_to_joints([-1.56832675,  0.39303148,  0.02632776, -1.98690212, -0.00319773,  2.35042797, 0.94667396])
 
 pose = panda.get_tcp_pose()
@@ -137,168 +71,81 @@ orien = panda.get_orientation()
 
 ################################################################################
 
-
-
+def get_marker():
+    res =  marker_detector.estimate_pose()
+    if res is None:
+        return None
+    else:
+        return res[:3, 3]
+    
 def get_obs():
     # print("Getting observation")
     pose = panda.get_tcp_pose()
-    goal = marker_detector.estimate_pose()
-    # read images from realsense and scale down
-    # read position of the robot ee
-    # save as dict
-    return {'goal': goal[:2, 3],
-            'agent_pos': pose[:2, 3]}
+    # goal = marker_detector.estimate_pose()
+    return pose[:3, 3]
 
+    return {'goal': goal[:3, 3],
+            'agent_pos': pose[:3, 3]}
+
+marker_stream = rx.interval(1.0/10.0, scheduler=rx.scheduler.NewThreadScheduler()) \
+    .pipe(ops.map(lambda _: get_marker())) \
+    .pipe(ops.filter(lambda x: x is not None))
     
 # Discard first frames
-get_obs()
+# get_obs()
 
 
-obs_deque = collections.deque(maxlen=obs_horizon)
-obs_deque.append(get_obs())
-time.sleep(0.2)
-obs_deque.append(get_obs())
+obs_deque = collections.deque(maxlen=policy.params.obs_horizon)
+# obs_deque.append(get_obs())
+# time.sleep(0.2)
+# obs_deque.append(get_obs())
 obs_stream = rx.interval(1.0/10.0, scheduler=rx.scheduler.NewThreadScheduler()) \
     .pipe(ops.map(lambda _: get_obs())) \
-    .pipe(ops.share()) \
+    .pipe(ops.share()) 
+
+
+combined_stream = rx.combine_latest(marker_stream, obs_stream) \
+    .pipe(ops.map(lambda x: {"goal": x[0], "agent_pos": x[1]})) \
     .subscribe(lambda x: obs_deque.append(x))
     
 
 while True:
-    # get first observation
-    # obs = get_obs()
-
-    # keep a queue of last 2 steps of observations
-    # obs_deque = collections.deque(
-    #     [obs] * obs_horizon, maxlen=obs_horizon)
-    # save visualization and rewards
-    rewards = list()
     done = False
     step_idx = 0
 
     while not done:
-        B = 1
-        # stack the last obs_horizon number of observations
-        # image_top = np.stack([x['image_top'] for x in obs_deque])
-        agent_poses = np.stack([x['agent_pos'] for x in obs_deque])
-        goal = obs_deque[-1]['goal']
-        # normalize observation
-        nagent_poses = normalize_data(agent_poses, stats=stats['states'])
-        ngoal = normalize_data(goal, stats=stats['goals'])
- 
-        # device transfer
-        nagent_poses = torch.from_numpy(nagent_poses).to(device, dtype=torch.float32)
-        ngoal = torch.from_numpy(ngoal).to(device, dtype=torch.float32)
-  
 
-        # infer action
-        with torch.no_grad():
+        # wait for obs_deque to have len 2
+        while len(obs_deque) < 2:
+            time.sleep(0.1)
+            print("Waiting for observation")
 
-            # reshape observation to (B,obs_horizon*obs_dim)
-            obs_cond = nagent_poses.unsqueeze(0).flatten(start_dim=1)
+        # print(obs_deque)
+        action = policy.infer_action(obs_deque)
 
-            # import pdb; pdb.set_trace() 
-            obs_cond =torch.cat([ngoal.unsqueeze(dim=0), obs_cond], dim=-1)
-
-            # initialize action from Guassian noise
-            noisy_action = torch.randn((B, pred_horizon, action_dim), device=device)
-            naction = noisy_action
-
-            # init scheduler
-            noise_scheduler.set_timesteps(num_diffusion_iters)
-
-            for k in noise_scheduler.timesteps:
-                # predict noise
-                # noise_pred = ema_nets['noise_pred_net'](
-                #     sample=naction,
-                #     timestep=k,
-                #     global_cond=obs_cond
-                # )
-                noise_pred = ema_nets(
-                    sample=naction,
-                    timestep=k,
-                    global_cond=obs_cond
-                )
-
-                # inverse diffusion step (remove noise)
-                naction = noise_scheduler.step(
-                    model_output=noise_pred,
-                    timestep=k,
-                    sample=naction
-                ).prev_sample
-
-        # unnormalize action
-        naction = naction.detach().to('cpu').numpy()
-        # (B, pred_horizon, action_dim)
-        naction = naction[0]
-        action_pred = unnormalize_data(naction, stats=stats['actions'])
-
-        # only take action_horizon number of actions
-        start = obs_horizon - 1
-        end = start + action_horizon
-        action = action_pred[start:end,:]
-        # (action_horizon, action_dim)
-
-        # execute action_horizon number of steps
-        # without replanning
-
-        # create waypoints from the action
-        # action = [(0.0, -0.4), (0.0, -0.41), (0.0, -0.42), (0.3, -0.43), (0.0, -0.44), (0.0, -0.45)]
         points = []
         for i in action:
-            tp = [i[0], i[1], trans[2]]
+            tp = [i[0], i[1], i[2]]
             points.append(tp)
+        
         points = np.array(points)
-        dists = np.linalg.norm(points[1:] - points[:-1], axis=1)
-        # check if last point is clsoe to the first point
-        too_close = np.linalg.norm(points[0] - points[-1]) < 0.005
-        if too_close:
-            points = points[:-1]
-        else:
-            points = points[np.where(dists > 0.005)]
+        # dists = np.linalg.norm(points[1:] - points[:-1], axis=1)
+        # # check if last point is clsoe to the first point
+        # too_close = np.linalg.norm(points[0] - points[-1]) < 0.005
+        # if too_close:
+        #     points = points[:-1]
+        # else:
+        #     points = points[np.where(dists > 0.005)]
 
         waypoints = []
         for point in points:
             waypoints.append(to_affine(point, orien))
-        # if too_close:
-        #     print("Waypoints are close. Using waypoint motion")
-        #     panda.recover_from_errors()
-        #     panda.waypoints(waypoints)
-        # else:
+
         panda.recover_from_errors()
-        if len(waypoints) > 2:
-            print("Waypoints are far. Using path motion")
-            panda.path(waypoints, blend=0.4)
-        elif len(waypoints) >= 1:
-            print("Waypoints are close. Using waypoint motion")
-            panda.waypoints(waypoints)
+        # if len(waypoints) > 2:
+        #     print("Waypoints are far. Using path motion")
+        #     panda.path(waypoints, blend=0.8)
+        # elif len(waypoints) >= 1:
+        #     print("Waypoints are close. Using waypoint motion")
+        panda.waypoints(waypoints)
 
-
-        # robot_motion = WaypointMotion([Waypoint(x, 0.0, Waypoint.Absolute) for x in waypoints])
-
-        # obs = get_obs()
-
-        # for i in range(len(action)):
-          
-        #     tp = [action[i][0], action[i][1], trans[2]]
-        #     waypoint = to_affine(tp, orien)
-        #     robot_motion = PathMotion([Waypoint(waypoint, 0.0, Waypoint.Absolute)])
-        #     panda.frankx.move(robot_motion)
-        #     obs = get_obs()
-        #     print("Action: ", action[i])
-
-        #     obs_deque.append(obs)
-
-        #     step_idx += 1
-        #     if step_idx > max_steps:
-        #         done = True
-        #     if done:
-        #         break
-
-# print out the maximum target coverage
-print('Score: ', max(rewards))
-
-# # visualize
-# from IPython.display import Video
-# vwrite('vis.mp4', imgs)
-# Video('vis.mp4', embed=True, width=256, height=256)
