f1deb10b678eacfd2bda9cdb7bfa95ab981a2227
krishanrana <krishanrana100@gmail.com>
2024-02-28 12:43:14 +1000

    implemented object centric action space

---
+++ diffrobot/diffusion_policy/datasets/vision_dataset.py
@@ -0,0 +1,124 @@
+
+import numpy as np
+import torch
+import pdb
+import os
+from torchvision.io import read_image
+from diffrobot.diffusion_policy.utils.dataset_utils import create_sample_indices, get_data_stats, normalize_data
+
+
+
+# dataset
+class DiffusionImageDataset(torch.utils.data.Dataset):
+    def __init__(self,
+                 dataset_path: str,
+                 pred_horizon: int,
+                 obs_horizon: int,
+                 action_horizon: int,
+                 phase: str,
+                 use_object_centric: bool,
+                 transform):
+        
+        self.dataset_path = dataset_path
+        self.all_states = np.load(f'{dataset_path}/all_states.pkl', allow_pickle=True)
+        self.transform = transform
+        self.phase = phase
+
+        indices = create_sample_indices(
+            sequence_length=pred_horizon,
+            dataset_path=dataset_path)
+        
+        # shuffle indices
+        np.random.seed(0)
+        np.random.shuffle(indices)
+        
+        self.index_order = indices.copy()
+        
+        # split into train and val
+        if self.phase == 'train':
+            indices = indices[:int(0.9*len(indices))]
+        elif self.phase == 'val':
+            indices = indices[int(0.9*len(indices)):]
+
+        # compute statistics and normalized data to [-1,1]
+        stats = dict()
+        stats["states"] = get_data_stats(self.all_states)
+        stats["actions"] = get_data_stats(self.all_states)
+        stats["images"] = {
+            'min': 0,
+            'max': 255
+        }
+    
+        self.indices = indices
+        self.stats = stats
+        self.pred_horizon = pred_horizon
+        self.action_horizon = action_horizon
+        self.obs_horizon = obs_horizon
+
+    def sample_sequence_images(self, dataset_path: str, states: list, episode: int, start_idx: int, end_idx: int):
+
+        #Paths to images
+        img_dir_front = os.path.join(dataset_path, "episodes", str(episode), "images", "front")
+        img_dir_hand = os.path.join(dataset_path, "episodes", str(episode), "images", "hand")
+
+        # Initialize lists to store slices
+        f_front = []
+        f_hand = []
+
+        start = start_idx+1
+        end = start + self.obs_horizon
+
+        for idx in range(start, end):
+            img_front = read_image(f'{img_dir_front}/{idx}.png')
+            img_hand = read_image(f'{img_dir_hand}/{idx}.png')
+
+            f_hand.append(img_hand)
+            f_front.append(img_front)
+
+        f_hand = torch.stack(f_hand, dim=0)
+        f_front = torch.stack(f_front, dim=0)
+
+        data = {
+            'image_hand': f_hand,
+            'image_front': f_front,
+            'robot_state': states[start_idx:end_idx],
+            'action': states[start_idx+1:end_idx+1]
+        }
+
+        return data
+
+
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, idx):
+        # get the start/end indices for this datapoint
+        episode, sample_start_idx, sample_end_idx = self.indices[idx]
+
+        # get nomralized data using these indices
+        nsample = self.sample_sequence_images(
+            dataset_path=self.dataset_path,
+            states=self.all_states[episode],
+            episode=episode,
+            start_idx=sample_start_idx,
+            end_idx=sample_end_idx)
+        
+        # normalize data
+        nsample['robot_state'] = normalize_data(nsample['robot_state'], self.stats['states'])
+        nsample['action'] = normalize_data(nsample['action'], self.stats['actions'])
+        nsample['image_hand'] = nsample['image_hand'] / 255.0
+        nsample['image_front'] = nsample['image_front'] / 255.0
+
+        nsample['image_hand'] = nsample['image_hand'][:self.obs_horizon,:]
+        nsample['image_front'] = nsample['image_front'][:self.obs_horizon,:]
+        nsample['robot_state'] = nsample['robot_state'][:self.obs_horizon,:]
+
+        nsample['image_hand'] = torch.stack([self.transform(img) for img in nsample['image_hand']])
+        nsample['image_front'] = torch.stack([self.transform(img) for img in nsample['image_front']])
+
+        return nsample
+
+
+
+
---
+++ diffrobot/diffusion_policy/utils/dataset_utils.py
@@ -0,0 +1,235 @@
+import os
+import pdb
+import cv2
+import json
+import numpy as np
+import tqdm
+from scipy.spatial.transform import Rotation as R
+import pickle
+
+
+def create_sample_indices(sequence_length:int,
+                          dataset_path:str):
+    
+    # iterate through all the episode folders
+    indices = list()
+
+    # sort numerically the episodes based on folder names
+    episodes = sorted(os.listdir(os.path.join(dataset_path, "episodes")), key=lambda x: int(x))
+    for episode in episodes:
+        # read the state.json file which consists of a list of dictionaries
+        state = json.load(open(os.path.join(dataset_path, "episodes", episode, "state.json")))
+
+        # get the length of the episode
+        episode_length = len(state)
+        # iterate through the episode
+        range_idx = episode_length - (sequence_length + 2)
+        for idx in range(range_idx):
+            # get the start and end index of the sequence
+            buffer_start_idx = idx
+            buffer_end_idx = idx + sequence_length
+            indices.append([int(episode), buffer_start_idx, buffer_end_idx])
+            assert buffer_end_idx - buffer_start_idx == sequence_length
+        
+    indices = np.array(indices)
+    return indices
+
+
+
+def sample_sequence_states(dataset_path: str, states: list, goals: list, episode: int, start_idx: int, end_idx: int):
+
+    data = {
+        # 'image_top': f_top,
+        'goal': goals[episode],
+        'robot_state': states[start_idx:end_idx],
+        'action': states[start_idx+1:end_idx+1]
+    }
+    return data
+
+
+
+def flatten_2d_lists(list_of_lists):
+    flattened_list = []
+    for sublist in list_of_lists:
+        for item in sublist:
+            flattened_list.append(item)
+    return flattened_list
+
+
+def get_data_stats(data: list):
+    data = np.array(flatten_2d_lists(data))
+    stats = {
+       'min': np.min(data, axis=0),
+       'max': np.max(data, axis=0)
+    }
+    return stats
+
+
+def normalize_data(data, stats):
+    # nomalize to [0,1]
+    ndata = (data - stats['min']) / (stats['max'] - stats['min'])
+    # normalize to [-1, 1]
+    ndata = ndata * 2 - 1
+    return ndata
+
+def unnormalize_data(ndata, stats):
+    ndata = (ndata + 1) / 2
+    data = ndata * (stats['max'] - stats['min']) + stats['min']
+    return data
+
+
+# create an image based dataset from video
+def decode_video(dataset_path:str):
+    state = []
+   # sort numerically the episodes based on folder names
+    episodes = sorted(os.listdir(os.path.join(dataset_path, "episodes")), key=lambda x: int(x))
+    for episode in episodes:
+            # Paths to the video files
+        vp_front = os.path.join(dataset_path, "episodes", str(episode), "video", "1.mp4")
+        # vp_left = os.path.join(dataset_path, "episodes", str(episode), "video", "2.mp4")
+        vp_hand = os.path.join(dataset_path, "episodes", str(episode), "video", "0.mp4")
+
+        # make images dir
+        img_dir = os.path.join(dataset_path, "episodes", str(episode), "images")
+        os.makedirs(img_dir, exist_ok=True)
+        
+        # make dir for each stream
+        # os.makedirs(os.path.join(img_dir, "top"), exist_ok=True)
+        os.makedirs(os.path.join(img_dir, "front"), exist_ok=True)
+        os.makedirs(os.path.join(img_dir, "hand"), exist_ok=True)
+
+        # decode video and resize images 
+        # os.system(f"ffmpeg -i {vp_top} -vf fps=10 -s 320x180 {img_dir}/top/%d.png")
+        os.system(f"ffmpeg -i {vp_front} -vf fps=10 -s 320x180 {img_dir}/front/%d.png")
+        os.system(f"ffmpeg -i {vp_hand} -vf fps=10 -s 320x180 {img_dir}/hand/%d.png")
+    
+    print("Decoding done")
+
+
+
+def detect_aruco_markers(dataset_path:str):
+    intrinsics_fpath = os.path.join(dataset_path, "cam_front_intrinsics.json")
+    extrinsics_fpath = os.path.join(dataset_path, "cameras.yaml")
+    with open(intrinsics_fpath, 'r') as f:
+        intrinsics = np.array(json.load(f))
+
+    # sort numerically the episodes based on folder names
+    episodes = sorted(os.listdir(os.path.join(dataset_path, "episodes")), key=lambda x: int(x))
+    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
+    parameters = cv2.aruco.DetectorParameters_create()
+
+    for episode in tqdm.tqdm(episodes):
+        # Paths to the image files
+        video_dir = os.path.join(dataset_path, "episodes", str(episode), "video", "1.mp4")
+
+        # read frames
+        cap = cv2.VideoCapture(video_dir)
+        tvecs = None
+        ret = True
+        while ret:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # detect markers
+            corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
+
+            if ids is None:
+                print("No markers found for episode ", episode)
+                continue
+
+            if 8 in ids:
+                idx = np.where(ids == 8)[0][0]
+                corners = np.array([corners[idx]])
+                ids = np.array([ids[idx]])
+            
+                frame = cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+
+                # detect and draw the pose
+                distcoeffs = np.array([-5.78085221e-02,  6.55928180e-02,  8.11965656e-05,  4.67534643e-04,
+       -2.07200460e-02])
+
+                rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 0.05, intrinsics, distcoeffs)
+                for i in range(len(rvecs)):
+                    frame = cv2.aruco.drawAxis(frame, intrinsics, distcoeffs, rvecs[i], tvecs[i], 0.1)
+
+            else:
+                print("No marker 8 found for episode ", episode)
+                continue
+                
+
+            #show frame
+            cv2.imshow("frame", frame)
+            cv2.waitKey(1)
+
+            r = R.from_rotvec(np.array(rvecs[0]).flatten())
+            T_cam_marker = np.eye(4)
+            T_cam_marker[:3, 3] = np.array(tvecs).flatten()
+            T_cam_marker[:3, :3] = r.as_matrix()
+
+
+
+            #save marker position as json
+            with open(os.path.join(dataset_path, "episodes", str(episode), "marker_pose.json"), 'w') as f:
+                json.dump(T_cam_marker.tolist(), f)
+            break
+
+        cap.release()
+
+    print("Done detecting markers")
+
+    return
+
+
+def extract_robot_positions(poses: list):
+    xyz = []
+    for episode in poses:
+        temp = []
+        for pose in episode:
+            temp.append(pose[:3, 3])
+        xyz.append(temp)
+    return xyz
+
+
+def extract_goal_positions(poses: list):
+    xyz = []
+    for episode in poses:
+        xyz.append(episode[:3, 3])
+    return xyz
+
+
+def extract_robot_poses(dataset_path:str):
+    state_poses = []
+   # sort numerically the episodes based on folder names
+    episodes = sorted(os.listdir(os.path.join(dataset_path, "episodes")), key=lambda x: int(x))
+    for episode in episodes:
+        # read the state.json file which consists of a list of dictionaries
+        raw_data = json.load(open(os.path.join(dataset_path, "episodes", episode ,"state.json")))
+        temp_poses = []
+        for idx in range(len(raw_data)):
+            pose = np.array(raw_data[idx]["X_BE"])
+            temp_poses.append(pose)
+        
+        state_poses.append(temp_poses)
+    return state_poses
+
+
+def extract_goal_poses(dataset_path:str):
+    goal_poses = []
+   # sort numerically the episodes based on folder names
+    episodes = sorted(os.listdir(os.path.join(dataset_path, "episodes")), key=lambda x: int(x))
+    for episode in episodes:
+        # read the state.json file which consists of a list of dictionaries
+        goal = np.array(json.load(open(os.path.join(dataset_path, "episodes", episode ,"marker_pose.json"))))
+        goal_poses.append(goal)
+    return goal_poses
+
+
+
+fpath = "/home/krishan/work/2024/datasets/franka_3D_reacher"
+# detect_aruco_markers(fpath)
+# decode_video(fpath)
+# out = extract_robot_poses(fpath)
+# out = extract_goal_poses(fpath)
+
+  
