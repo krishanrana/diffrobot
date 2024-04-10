855d6c7d1225764e9274bb8831057adfce9fdda2
Krishan Rana <krishanrana100@gmail.com>
2024-04-11 08:48:08 +1000

    inference unconstrained object

--- diffrobot/diffusion_policy/inference/inference_real_robot_state.py
+++ diffrobot/diffusion_policy/inference/inference_real_robot_state.py
@@ -197,7 +197,7 @@ class RobotInferenceController:
 
 
 # Example usage
-controller = RobotInferenceController(saved_run_name='fluent-salad-57_state',
+controller = RobotInferenceController(saved_run_name='desert-jazz-60_state',
                                       robot_ip='172.16.0.2', 
                                       sensor_ip='131.181.33.191', 
                                       sensor_port=5000)
