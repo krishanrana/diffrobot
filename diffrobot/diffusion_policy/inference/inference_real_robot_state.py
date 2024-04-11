e847463f9ba719b27887d7f490f60cd7a176399c
Krishan Rana <krishanrana100@gmail.com>
2024-04-11 16:35:59 +1000

    inference unconstrained object

--- diffrobot/diffusion_policy/inference/inference_real_robot_state.py
+++ diffrobot/diffusion_policy/inference/inference_real_robot_state.py
@@ -18,6 +18,15 @@ from diffrobot.robot.visualizer import RobotViz
 import pdb
 
 
+def read_X_BF(s) -> np.ndarray:
+    import spatialmath as sm # for poses
+    X_BE = np.array(s.O_T_EE).reshape(4, 4).astype(np.float32).T
+    X_FE = np.array(s.F_T_EE).reshape(4, 4).astype(np.float32).T
+    X_EF = np.linalg.inv(X_FE)
+    X_BF = X_BE @ X_EF 
+    return X_BF
+
+
 class RobotInferenceController:
     def __init__(self, saved_run_name, robot_ip, sensor_ip, sensor_port, record_fps=30):
         self.saved_run_name = saved_run_name
@@ -61,7 +70,7 @@ class RobotInferenceController:
         # self.panda.set_dynamic_rel(1.0, accel_rel=0.2, jerk_rel=0.05)
         # self.panda.set_dynamic_rel(0.4, accel_rel=0.005, jerk_rel=0.05)
 
-        self.panda.set_dynamic_rel(1.0, accel_rel=0.0005, jerk_rel=0.05)
+        self.panda.set_dynamic_rel(0.1, accel_rel=0.2, jerk_rel=0.05)
         self.panda.frankx.set_collision_behavior(
 			[30.0, 30.0, 30.0, 30.0, 30.0, 30.0, 30.0],
 			[30.0, 30.0, 30.0, 30.0, 30.0, 30.0, 30.0],
@@ -78,55 +87,40 @@ class RobotInferenceController:
         self.load_transforms()
 
     def load_transforms(self):
+        self.X_BO = None
         with open(f'../runs/{self.saved_run_name}/transforms.json', 'r') as f:
             trans = json.load(f)
             self.X_FC = np.array(trans['X_FC'])
-            self.X_FE = np.array(trans['X_FE'])
-
-        self.X_BO = self.estimate_marker_pose()
-
-        if self.X_BO is None:
-            print('Marker not found. Human intervention required...')
-            pdb.set_trace()
-
-
-    def estimate_marker_pose(self):
-        X_CO = self.marker_detector.estimate_pose()
-        if X_CO is None:
-            return None
-        X_BE = self.panda.get_tcp_pose()
-        X_BF = np.dot(X_BE, np.linalg.inv(self.X_FE))
-        X_BC = np.dot(X_BF, self.X_FC)
-        X_BO = np.dot(X_BC, X_CO)
-        self.robot_visualiser.object_pose.T = X_BO
-        return X_BO
 
     def get_marker(self):
         res = self.marker_detector.estimate_pose()
         return None if res is None else res
 
     def get_obs(self):
-        X_BE = self.panda.get_tcp_pose()
-        # tactile_sensor = self.sensor_socket.get_forces()
-        joint_torques = self.panda.get_joint_torques()
-        ee_forces = self.panda.get_ee_forces()
+        s = self.panda.get_state()
+        X_BE = np.array(s.O_T_EE).reshape(4,4).T
+        X_BF = read_X_BF(s)
+        X_CO = self.marker_detector.estimate_pose()
+        if X_CO is not None:
+            X_BC = X_BF @ self.X_FC
+            X_BO = X_BC @ X_CO
 
-        temp = self.marker_detector.estimate_pose()
-        if temp is not None:
-            self.X_BO = temp
+            dist = np.dot(np.array([0,0,1]), X_BO[:3,2])
+            if dist > 0.99: # filter out bad object poses
+                self.X_BO = X_BO
+            
+        self.robot_visualiser.object_pose.T = self.X_BO
 
         return {"X_BE": X_BE, 
-                "X_BO": self.X_BO, 
-                # "tactile_sensor": tactile_sensor, 
-                "joint_torques": joint_torques, 
-                "ee_forces": ee_forces}
+                "X_BO": self.X_BO }
     
     def start_inference(self):
         obs_stream = rx.interval(1.0/10.0, scheduler=rx.scheduler.NewThreadScheduler()) \
             .pipe(ops.map(lambda _: self.get_obs())) \
+            .pipe(ops.filter(lambda x: x["X_BO"] is not None)) \
             .subscribe(lambda x: self.obs_deque.append(x))  
       
-        motion = self.panda.start_impedance_controller(200, 50, 5)
+        motion = self.panda.start_impedance_controller(200, 30, 5)
 
         while True:
             done = False
@@ -146,7 +140,9 @@ class RobotInferenceController:
 
                 waypoints = []
                 # self.panda.recover_from_errors()
-                for i in range(len(self.action)):
+                # take very 3rd action
+                # for i in range(len(self.action)):
+                for i in range(0, len(self.action), 3):
                     print('Task Progress: ', self.progress[i])
                     # if self.progress[i] >  0.85:
                     #     print('I think im done with the task!')
@@ -154,7 +150,7 @@ class RobotInferenceController:
                     #     self.progress = np.zeros((self.params.action_horizon))
 
                     trans, orien = matrix_to_pos_orn(self.action[i])
-                    # motion.set_target(to_affine(trans, orien))
+                    motion.set_target(to_affine(trans, orien))
 
                     robot_q = self.panda.get_joint_positions()
                     self.robot_visualiser.ee_pose.T = self.panda.get_tcp_pose()
@@ -162,7 +158,7 @@ class RobotInferenceController:
                     self.robot_visualiser.step(robot_q)
                     time.sleep(0.1)
 
-                    pdb.set_trace()
+                    # pdb.set_trace()
                     
 
                     # waypoints.append(to_affine(trans, orien))
@@ -171,7 +167,7 @@ class RobotInferenceController:
 
                 # self.panda.recover_from_errors()
                 # for i, waypoint in enumerate(waypoints):
-                #     motion.set_target(waypoint)
+                #     # motion.set_target(waypoint)
 
                 #     robot_q = self.panda.get_joint_positions()
 
@@ -179,13 +175,7 @@ class RobotInferenceController:
                 #     self.robot_visualiser.policy_pose.T = self.action[i]    
                 #     self.robot_visualiser.step(robot_q)
                     
-                    # self.key = cv2.waitKey(100) & 0xFF
-                    # if self.key == self.toggle_key:
-                    #     print('Key has been toggled')
-                    #     self.reset_progress = True
-                    #     #self.obs_deque.clear()
-                        
-                    #     break
+
                 # self.robot_visualiser.ee_pose.T = self.panda.get_tcp_pose()
                 # self.robot_visualiser.policy_pose.T = self.action[-1]  
                 # robot_q = self.panda.get_joint_positions()  
